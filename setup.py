df2bce3317501245fb10c2320ef6f49e7cfb52e7
jneilliii <jneilliii+github@gmail.com>
2023-06-06 11:56:01 -0400

    Update setup.py
    hopefully fix issues related to pydrive2 dependency not installing properly

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/jneilliii/OctoPrint-GoogleDriveFiles"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["pydrive2", "treelib"]
+plugin_requires = ["cryptography>=38.0.0,<40", "pydrive2", "treelib"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
